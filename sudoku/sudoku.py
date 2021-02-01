aa1d54b873ed63bfabb20d2ba807d926dacfe5d5
Terrence Kim <terrence.kim@gmail.com>
2021-02-01 16:30:11 -0500

    update

--- sudoku/sudoku.py
+++ sudoku/sudoku.py
@@ -8,14 +8,14 @@ def find_next_empty(puzzle):
             if puzzle[r][c] == -1:
                 return r, c
     
-    return None, None # if no spaces in the puzzle are empty (-1)
+    return None, None  # if no spaces in the puzzle are empty (-1)
 
 def is_valid(puzzle, guess, row, col):
     # figures out whether the guess at the row/col of the puzzle is a valid guess
     # returns True or False
     
     # for a guess to be valid, then we need to follow the sudoku rules
-    # that number must not be repated in the row, column, or 3x3 square that it appears in
+    # that number must not be repeated in the row, column, or 3x3 square that it appears in
     
     # let's start with the row
     row_vals = puzzle[row]
@@ -24,18 +24,18 @@ def is_valid(puzzle, guess, row, col):
     
     # now the column
     # col_vals = []
-    # for i in range[9]:
-    #   col_vals.append(puzzle[i][col])
+    # for i in range(9):
+    #     col_vals.append(puzzle[i][col])
     col_vals = [puzzle[i][col] for i in range(9)]
     if guess in col_vals:
         return False
     
     # and then the square
-    row_start = (row // 3) * 3 # 10//3 = 3, 5//3 = 1, 1 //3 = 0
+    row_start = (row // 3) * 3 # 10 // 3 = 3, 5 // 3 = 1, 1 //3 = 0
     col_start = (col // 3) * 3
     
     for r in range(row_start, row_start + 3):
-        for c in range(col_start, col_start +3):
+        for c in range(col_start, col_start + 3):
             if puzzle[r][c] == guess:
                 return False
     
@@ -47,11 +47,11 @@ def solve_sudoku(puzzle):
     # return whether a solution exists
     # mutates puzzle to be the solution (if solution exists)
     
-    # step1: chose somewhere on the puzzle to make a guess
+    # step 1: choose somewhere on the puzzle to make a guess
     row, col = find_next_empty(puzzle)
     
-    # step1.1: if there's nowhere left, then we're done because we only allowed valid inputs
-    if row is None: # this is true if our find_next_empty function returns None, None
+    # step 1.1: if there's nowhere left, then we're done because we only allowed valid inputs
+    if row is None:  # this is true if our find_next_empty function returns None, None
         return True
     
     # step 2: if there is a place to put a number, then make a guess between 1 and 9
@@ -60,7 +60,7 @@ def solve_sudoku(puzzle):
         if is_valid(puzzle, guess, row, col):
             # step 3.1: if this is a valid guess, then place it at that spot on the puzzle
             puzzle[row][col] = guess
-            # step 4: then we recursively call our solver:
+            # step 4: then we recursively call our solver!
             if solve_sudoku(puzzle):
                 return True
         
@@ -72,17 +72,17 @@ def solve_sudoku(puzzle):
 
 if __name__ == '__main__':
     example_board = [
-        [3, 9, -1,  -1, 5, -1,  -1, -1, -1],
-        [-1, -1, -1,  2, -1, -1  -1, -1, 5],
-        [-1, -1, -1,  7, 1, 9,  -1, 8, -1],
+        [3, 9, -1,   -1, 5, -1,   -1, -1, -1],
+        [-1, -1, -1,   2, -1, -1,   -1, -1, 5],
+        [-1, -1, -1,   7, 1, 9,   -1, 8, -1],
         
-        [-1, 5, -1,  -1, 6, 8,  -1, -1, -1],
-        [2, -1, 6,  -1, -1, 3,  -1, -1, -1],
-        [-1, -1, -1,  -1, -1, -1,  -1, -1, 4],
+        [-1, 5, -1,   -1, 6, 8,   -1, -1, -1],
+        [2, -1, 6,   -1, -1, 3,   -1, -1, -1],
+        [-1, -1, -1,   -1, -1, -1,   -1, -1, 4],
         
-        [5, -1, -1,  -1, -1, -1,  -1, -1, -1],
-        [6, 7, -1,  1, -1, 5,  -1, 4, -1],
-        [1, -1, 9,  -1, -1, -1,  2, -1, -1],
+        [5, -1, -1,   -1, -1, -1,   -1, -1, -1],
+        [6, 7, -1,   1, -1, 5,   -1, 4, -1],
+        [1, -1, 9,   -1, -1, -1,   2, -1, -1]
     ]
     
     print(solve_sudoku(example_board))
