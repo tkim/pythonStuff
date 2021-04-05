eec8f2c7e3d2f4b6594f39e1df6008d3cf44b048
Terrence Kim <terrence.kim@gmail.com>
2021-04-05 09:15:22 -0400

    crypto visualization

---
+++ context_manager/context_manager.py
@@ -0,0 +1,6 @@
+with open('snp500.csv', 'r') as f:
+    file_contents = f.read()
+
+words = file_contents.split('  ')
+word_count = len(words)
+print(word_count)
---
+++ crypto_visualization/bitcoin_vis.py
@@ -0,0 +1,38 @@
+import pandas_datareader as web
+import matplotlib.pyplot as plt
+import mplfinance as mpf 
+import datetime as dt 
+
+crypto = "BTC"
+currency = "USD"
+
+start = dt.datetime(2020, 1, 1)
+end = dt.datetime.now()
+
+# Pandas DataReader data source options 
+# yahoo, google, fred (St. Louise Fed), famafrench (Kenneth French's data library)
+# World bank  
+
+data = web.DataReader(f"{crypto}-{currency}", "yahoo", start, end)
+
+# for creating multiple currency charts
+btc = web.DataReader(f"{crypto}-{currency}", "yahoo", start, end)
+eth = web.DataReader(f"ETH-{currency}", "yahoo", start, end)
+
+# to make the chart logrithimic
+plt.yscale("log")
+
+# print(data)
+
+# basic line chart on Close
+# plt.plot(data['Close'])
+# plt.show()
+
+# plot candlestick 
+# mpf.plot(data, type="candle", volume=True, style="yahoo")  
+
+# for multiple currency charts
+plt.plot(btc['Close'], label="BTC")
+plt.plot(eth['Close'], label="ETH")
+plt.legend(loc="upper left")
+plt.show()
