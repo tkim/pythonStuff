c22295aa3e1f5576fcef75ef1bf7788e65b933f8
Terrence Kim <terrence.kim@gmail.com>
2021-05-18 14:49:07 -0400

    Update python_to_exe.py

--- python_to_exe/python_to_exe.py
+++ python_to_exe/python_to_exe.py
@@ -1,4 +1,8 @@
 from distutils.core import setup
 import py2exe
 
+# use the name of the file you want to conver to .exe and go to the directory where the .py file exists.
 setup(console=['password_generator.py'])
+
+# to create the executable run the following 
+# py -3 python_to_exe.py py2exe
