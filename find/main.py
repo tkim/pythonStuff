87011bb36e23472da449f8f8ed7b3f84a6a11334
Terrence Kim <terrence.kim@gmail.com>
2022-01-29 08:49:32 -0500

    update

---
+++ find/main.py
@@ -0,0 +1,6 @@
+names = ['Peter Parker', 'Clark Kent', 'Wade Wilson', 'Bruce Wayne']
+heroes = ['Spiderman', 'Superman', 'Deadpool', 'Batman']
+
+for index, name in enumerate(names):
+    hero = heroes[index]
+    print(f'{name} is actually {hero}')
