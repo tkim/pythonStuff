7a3fb635a31f695915238b0db69b85c57f2a4f2f
Terrence Kim <terrence.kim@gmail.com>
2022-02-01 08:18:57 -0500

    Update main.py

--- find/main.py
+++ find/main.py
@@ -1,6 +1,15 @@
+# old code
+# names = ['Peter Parker', 'Clark Kent', 'Wade Wilson', 'Bruce Wayne']
+# heroes = ['Spiderman', 'Superman', 'Deadpool', 'Batman']
+
+# for index, name in enumerate(names):
+#     hero = heroes[index]
+#     print(f'{name} is actually {hero}')
+
+# new code
 names = ['Peter Parker', 'Clark Kent', 'Wade Wilson', 'Bruce Wayne']
 heroes = ['Spiderman', 'Superman', 'Deadpool', 'Batman']
+universes = ['Marvel', 'DC', 'Marvel', 'DC']
 
-for index, name in enumerate(names):
-    hero = heroes[index]
-    print(f'{name} is actually {hero}')
+for name, hero, universe in zip(names, heroes, universes):
+    print(f'{name} is acutally {hero} from {universe}')
