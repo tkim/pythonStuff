d416f95ccdbcd869041bf1c414e872847a831070
Terrence Kim <terrence.kim@gmail.com>
2023-10-26 23:11:58 -0400

    update

---
+++ vector embedding/index.py
@@ -0,0 +1,10 @@
+# dependencies 
+# pip install cassio datasets langchain openai tiktoken
+
+ASTRA_DB_SECURE_BUNDLE_PATH = "<<PATH>>"
+ASTRA_DB_APPLICATION_TOKEN = "<<TOKEN>>"
+ASTRA_DB_CLIENT_ID = "TOKEN"
+ASTRA_DB_CLIENT_SECRET = " "
+ASTRA_DB_KEYSPACE = "<<KEYSPACE_NAME>>"
+OPENAI_API_KEY = "<<OPENAK_KEY>>"
+
---
+++ vector embedding/langchain test.py
@@ -0,0 +1,33 @@
+# turn word to numberic representation!
+
+# OpenAI API Key
+# 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
+
+# use OpenAI API to create embedding vector representing the input text.
+import os
+import openai
+
+openai.api_key = "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"
+openai.Embedding.create(
+  model="text-embedding-ada-002",
+  input="food"
+)
+
+# vector & database
+# create a vector db in datastax personal "free"
+
+# token details
+{
+  "clientId": "BazqnNEZOvWxRyKniNpxchaR"
+  "secret": "Zj-0a-gSxKaDz.l_5gHRS8LClfZ6JcGAoBSz+eMfcCj-CsubGiDYZNfP9NEjygiPn1ToaZrPEX2fH1yML39r4v7dZh1DcPha,5ITZ35BbFat2Q791RkZfbrmioA4lQi5"
+  "token": "AstraCS:BazqnNEZOvWxRyKniNpxchaR:3d0241494e6b19c051c986e9102049f26922971ec12c300ce8d9eee457488179"
+}
+
+https://datastax-cluster-config-prod.s3.us-east-2.amazonaws.com/f608c634-cb6b-4746-992c-7fa065feeea1-1/secure-connect-vector-database.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA2AIQRQ76S2JCB77W%2F20231001%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20231001T182528Z&X-Amz-Expires=300&X-Amz-SignedHeaders=host&X-Amz-Signature=18484debf3839fb6c286c5f74ced6b670734ff60285fd94c412666310f30d3f1
+
+# open ai key for demo
+# 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
+
+# langchain
+
+
